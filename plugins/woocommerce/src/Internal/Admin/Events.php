8725e53b4784068a95b817f683eee7400a08f6ff
Joshua Flowers <joshuatf@gmail.com>
2022-10-25 16:52:52 -0700

    Remove update store details note

--- plugins/woocommerce/src/Internal/Admin/Events.php
+++ plugins/woocommerce/src/Internal/Admin/Events.php
@@ -43,7 +43,6 @@ use \Automattic\WooCommerce\Internal\Admin\Notes\SellingOnlineCourses;
 use \Automattic\WooCommerce\Internal\Admin\Notes\TestCheckout;
 use \Automattic\WooCommerce\Internal\Admin\Notes\TrackingOptIn;
 use \Automattic\WooCommerce\Internal\Admin\Notes\UnsecuredReportFiles;
-use \Automattic\WooCommerce\Internal\Admin\Notes\UpdateStoreDetails;
 use \Automattic\WooCommerce\Internal\Admin\Notes\WelcomeToWooCommerceForStoreUsers;
 use \Automattic\WooCommerce\Internal\Admin\Notes\WooCommercePayments;
 use \Automattic\WooCommerce\Internal\Admin\Notes\WooCommerceSubscriptions;
@@ -101,7 +100,6 @@ class Events {
 		RealTimeOrderAlerts::class,
 		TestCheckout::class,
 		TrackingOptIn::class,
-		UpdateStoreDetails::class,
 		WooCommercePayments::class,
 		WooCommerceSubscriptions::class,
 	);
